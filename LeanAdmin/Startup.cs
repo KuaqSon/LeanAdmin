b4f038a7ec30ee69eabc84cfe5b1172191ff4ee2
KuaqSon <qson.ng@gmail.com>
2018-06-19 16:31:42 +0700

    Add project files.

---
+++ LeanAdmin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LeanAdmin.Startup))]
+namespace LeanAdmin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

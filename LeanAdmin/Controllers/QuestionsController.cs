c86c29a3f16bd0cb342b0912d6b4c12286a6f757
KuaqSon <qson.ng@gmail.com>
2018-06-20 10:58:21 +0700

    add page AddQuestions, pageList

--- LeanAdmin/Controllers/QuestionsController.cs
+++ LeanAdmin/Controllers/QuestionsController.cs
@@ -1,4 +1,5 @@
 ﻿using LeanAdmin.Models;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,24 +11,88 @@ namespace LeanAdmin.Controllers
     public class QuestionsController : Controller
     {
         // GET: Questions
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
-            List<Question> questions = new List<Question>
+            ViewBag.CurrentSort = sortOrder;
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            IEnumerable<Question> questions = new List<Question>
             {
                 new Question { Id = 1,
-                Label = "radio",
+                Label = "radio 1",
                 Description = "a radio control",
                 UpdatedDate = DateTime.Parse("1/1/2018"),
                 ControlId =1},
-                
                 new Question { Id = 2,
-                Label = "radio",
+                Label = "radio 2",
                 Description = "a radio control",
                 UpdatedDate = DateTime.Parse("1/1/2018"),
-                ControlId =1 }
-
+                ControlId =1 },
+                new Question { Id = 2,
+                Label = "radio 3",
+                Description = "a radio control",
+                UpdatedDate = DateTime.Parse("1/1/2018"),
+                ControlId =1 },
+                new Question { Id = 2,
+                Label = "radio 4",
+                Description = "a radio control",
+                UpdatedDate = DateTime.Parse("1/1/2018"),
+                ControlId =1 },
+                new Question { Id = 2,
+                Label = "radio 5",
+                Description = "a radio control",
+                UpdatedDate = DateTime.Parse("1/1/2018"),
+                ControlId =1 },
+                new Question { Id = 2,
+                Label = "radio 6",
+                Description = "a radio control",
+                UpdatedDate = DateTime.Parse("1/1/2018"),
+                ControlId =1 },
+                new Question { Id = 2,
+                Label = "radio 7",
+                Description = "a radio control",
+                UpdatedDate = DateTime.Parse("1/1/2018"),
+                ControlId =1 },
+                new Question { Id = 2,
+                Label = "radio 8",
+                Description = "a radio control",
+                UpdatedDate = DateTime.Parse("1/1/2018"),
+                ControlId =1 },
+                new Question { Id = 2,
+                Label = "radio 9",
+                Description = "a radio control",
+                UpdatedDate = DateTime.Parse("1/1/2018"),
+                ControlId =1 },
+                new Question { Id = 2,
+                Label = "radio 10",
+                Description = "a radio control",
+                UpdatedDate = DateTime.Parse("1/1/2018"),
+                ControlId =1 },
+                
             };
-            return View(questions);
+
+            questions = questions.OrderBy(x => x.Label);
+            var pageSize = 4;
+            var pageNumber = (page ?? 1);
+            var listQuestions = questions.ToPagedList(pageNumber, pageSize);
+            return View(listQuestions);
         }
+
+        public ActionResult AddQuestion(string Title)
+        {
+            return View();
+        }
+
+
+
     }
 }
